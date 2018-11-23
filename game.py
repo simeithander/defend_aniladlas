2ada02dae455dcd8b6748b36f2c744657455437b
Simei Thander <simeithander@gmail.com>
2018-11-23 20:41:49 -0300

    Correção de posicionamento do codigo.

--- game.py
+++ game.py
@@ -15,6 +15,8 @@ heroi_y = 425
 vel_x = 1
 vel_y = 1
 altura_pulo = 100
+heroi_imgs = []
+heroi_rect = []
 
 #blocos
 lista_chao = [33,97,161,225,289,353,417,481,545,609,673]
@@ -31,6 +33,12 @@ def events():
     if e.type == pygame.QUIT:
         pygame.quit()
         sys.exit()
+def heroi(heroi_x,heroi_y):
+    heroi = pygame.image.load("arquivos/player/player-idle/player-idle-1.png")
+    heroirect = heroi.get_rect()
+    heroirect.center = (heroi_x,heroi_y)
+    heroi.set_colorkey((255, 0, 255))
+    display.blit(heroi, heroirect)
 
 def lodo(lodo_x,lodo_y):
     lodo = pygame.image.load("arquivos/lodo.png")
@@ -39,40 +47,33 @@ def lodo(lodo_x,lodo_y):
     lodo.set_colorkey((255,0,255))
     display.blit(lodo, lodorect)
 
-def heroi(heroi_x,heroi_y):
-  heroi = pygame.image.load("arquivos/player/player-idle/player-idle-1.png")
-  heroirect = heroi.get_rect()
-  heroirect.center = (heroi_x,heroi_y)
-  heroi.set_colorkey((255, 0, 255))
-  display.blit(heroi, heroirect)
-
 def bloco(bloco_x, bloco_y):
-  bloco = pygame.image.load("arquivos/spt_bloco.png")
-  blocorect = bloco.get_rect()
-  blocorect.center = (bloco_x,bloco_y)
-  bloco.set_colorkey((255,0,255))
-  display.blit(bloco, blocorect)
+      bloco = pygame.image.load("arquivos/spt_bloco.png")
+      blocorect = bloco.get_rect()
+      blocorect.center = (bloco_x,bloco_y)
+      bloco.set_colorkey((255,0,255))
+      display.blit(bloco, blocorect)
 
 def controles(heroi):
-  global heroi_x, vel_x, heroi_y
-  keys = pygame.key.get_pressed()
-  if keys[pygame.K_LEFT]:
-       heroi_x -= vel_x
-       heroi(heroi_x,heroi_y)
-  if keys[pygame.K_RIGHT]:
-       heroi_x += vel_x
-       heroi(heroi_x,heroi_y)
-  if keys[K_UP] and player.jumping == False and player.jump_offset == 0:
-       player.jumping = True
+      global heroi_x, vel_x, heroi_y
+      keys = pygame.key.get_pressed()
+      if keys[pygame.K_LEFT]:
+           heroi_x -= vel_x
+           heroi(heroi_x,heroi_y)
+      if keys[pygame.K_RIGHT]:
+           heroi_x += vel_x
+           heroi(heroi_x,heroi_y)
+      if keys[K_UP] and player.jumping == False and player.jump_offset == 0:
+           player.jumping = True
 
 def pulando(player):
-  global altura_pulo
-  if player.jumping:
-    player.jump_offset += 5
-  if player.jump_offset >= altura_pulo:
-    player.jumping = False
-  elif player.jump_offset > 0 and player.jumping == False:
-    player.jump_offset -= 5
+      global altura_pulo
+      if player.jumping:
+        player.jump_offset += 5
+      if player.jump_offset >= altura_pulo:
+        player.jumping = False
+      elif player.jump_offset > 0 and player.jumping == False:
+        player.jump_offset -= 5
 
 def cenario():
     display.blit(background_img, (0, 0))
@@ -85,14 +86,14 @@ def cenario():
 
 #inicio
 while True:
-  eventos = pygame.event.get()
-  for e in eventos:
-    if e.type == pygame.QUIT:
-        pygame.quit()
-        sys.exit()
-    #print(e)
-  cenario()
-  heroi(heroi_x, heroi_y)
-  controles(heroi)
-
-  pygame.display.flip()
+    tempo = pygame.time.get_ticks()/1000
+    eventos = pygame.event.get()
+    for e in eventos:
+        if e.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        #print(e)
+    cenario()
+    heroi(heroi_x, heroi_y)
+    controles(heroi)
+    pygame.display.flip()
